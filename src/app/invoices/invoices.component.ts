a59f44b93b8cef9ed6e52507ca6f7ff024e1275c
curtis.sager <curtis.sager@oit.edu>
2019-01-29 19:18:21 -0800

    Added the invoice page UI elements, mock invoices, navbar colors.

--- src/app/invoices/invoices.component.ts
+++ src/app/invoices/invoices.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { InvoiceService } from './../_services/invoice.service';
+import { Invoice } from './../_models/invoice';
+import { INVOICES } from './../mock-invoices';
+
 
 @Component({
   selector: 'app-invoices',
@@ -6,10 +12,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invoices.component.css']
 })
 export class InvoicesComponent implements OnInit {
+  // invoiceId = new FormControl('');
+  invoices = INVOICES;
+  // invoice: Invoice;
 
-  constructor() { }
+  constructor(private invoiceService: InvoiceService) { }
 
   ngOnInit() {
   }
 
+  getInvoiceById(id) {
+    // this.invoiceService.getInvoiceById(id).subscribe(results => this.invoice = results);
+  }
+  getInvoices() {
+    // return this.invoiceService.getInvoices().subscribe(results => this.invoices = results);
+  }
 }
---
+++ src/app/mock-invoices.ts
@@ -0,0 +1,10 @@
+import { Invoice } from './_models/invoice';
+
+export const INVOICES: Invoice[] = [
+  {id: 21, businessId: 12, date: '02/15/2018', outgoingInv: 12, incomingInv: 0, totalcost: 21.54},
+  {id: 22, businessId: 11, date: '03/23/2018', outgoingInv: 0, incomingInv: 25, totalcost: 32.45},
+  {id: 23, businessId: 8, date: '05/15/2018', outgoingInv: 23, incomingInv: 10, totalcost: 76.86},
+  {id: 24, businessId: 4, date: '07/02/2018', outgoingInv: 0, incomingInv: 7, totalcost: 56.25},
+  {id: 25, businessId: 12, date: '11/20/2018', outgoingInv: 23, incomingInv: 0, totalcost: 86.53},
+  {id: 26, businessId: 14, date: '09/10/2018', outgoingInv: 7, incomingInv: 2, totalcost: 35.96},
+];
