d8a58072b46da4eef5258a978cf280d7dee92891
unknown <zelelrgabriel@yahoo.com>
2019-04-23 16:53:46 -0700

    edited new customer page

--- src/app/newcustomer/newcustomer.component.ts
+++ src/app/newcustomer/newcustomer.component.ts
@@ -14,17 +14,20 @@ export class NewcustomerComponent implements OnInit {
   public fname: '';
   public lname: '';
   public companyName: '';
-  public isCompany: false;
+  public isCompany: boolean = false;
   public phone: '';
   public email: '';
   public country: '';
-  public state: '';
+  public state: string;
   public zip: '';
   public city: '';
   public street1: '';
   public street2: '';
   public defaultAddress: true;
   public id: number;
+  public stateCodes = 
+  ['AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO',
+  'MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','PR','RI','SC','SD','TN','TX','UT','VT','VI','VA','WA','WV','WI','WY'];
 
   constructor(private customerService: CustomerService) {
    }
@@ -36,7 +39,7 @@ export class NewcustomerComponent implements OnInit {
     this.customer = new CustomerCreateDto(this.fname, this.lname, this.companyName, this.isCompany, this.phone, this.email,
       new AddressOnly(this.country, this.state, this.zip, this.city, this.street1, this.street2));
     this.customerService.addCustomer(this.customer).subscribe();
-    location.reload();
+    //location.reload();
   }
 
 }
