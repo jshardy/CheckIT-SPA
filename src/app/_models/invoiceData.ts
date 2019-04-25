bafd99e5968463eb9b01163b15879dde1d80b447
Joshua Hardy <joshua.hardy@oit.edu>
2019-04-24 23:53:17 -0700

    InvoiceData updated.

--- src/app/_models/invoiceData.ts
+++ src/app/_models/invoiceData.ts
@@ -1,8 +1,20 @@
+import { LineItemData } from './LineItemData';
+
 export interface InvoiceData {
   id?: number;
   invoiceDate?: string;
   outgoingInv?: Boolean;
   amountPaid?: number;
   invoiceCustID?: number;
-  itemList?: number[];
+  itemList?: LineItemData[];
 }
+
+
+// public class InvoiceData {
+//   public int Id { get; set; }
+//         public DateTime InvoiceDate { get; set; }
+//         public bool OutgoingInv { get; set; }
+//         public decimal AmountPaid { get; set; }
+//         public int InvoiceCustID { get; set; }
+//         public List < LineItemData > LineItemList { get; set; }
+//     }
