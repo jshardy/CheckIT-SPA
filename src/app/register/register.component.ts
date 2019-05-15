3b4ff48ef230885e7ccb29ff5a485c44d6438427
Joshua Hardy <joshua.hardy@oit.edu>
2019-05-15 05:23:49 -0700

    Fixed login error messages

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -17,9 +18,14 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
+    this.authService.register(this.model).subscribe((result) => {
       this.alertify.success('Registration Successful');
     }, error => {
+        if (error === "Username already exists")
+          console.log(error);
+        else
+          error = "Password must be 8 characters long";
+
       this.alertify.error(error);
     });
   }
